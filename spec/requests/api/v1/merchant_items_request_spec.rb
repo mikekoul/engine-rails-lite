194bd7506a9d744d429ab459d3e1de140af89a60
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-31 10:28:07 -0600

    feat/Add endpoint for all merchant items

---
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'Merchant Items API' do
+  it '#index' do
+
+    merchant = create(:merchant)
+    create_list(:item, 3, merchant_id: merchant.id)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(items[:data].count).to eq(3)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+      expect(item[:type]).to eq('item')
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+    end
+  end
+end
