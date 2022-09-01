c629a72099ab247d78add282cbd0edad0f72b2fe
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-01 14:45:58 -0600

    test/Add edge case extenstion param validation

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SearchesController < ApplicationController
+  before_action :param_validation
 
   def find_merchant
     merchant = Merchant.search_merchant(params[:name])
@@ -6,6 +7,13 @@ class Api::V1::SearchesController < ApplicationController
       render json: { data: { message: 'Merchant not found' } }
     else
       render json: MerchantSerializer.new(merchant)
+    end
+  end
+
+  private
+  def param_validation
+    if !params[:name].present?
+      render json: { data: {} }, status: 400
     end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe '#class methods' do
+    it 'returns a merchant with a case-insensitive and fragmented name search' do
+      merchant_1 = Merchant.create!(name: 'Little Shop of Horrors')
+      merchant_2 = Merchant.create!(name: 'Strange Imports')
+      merchant_3 = Merchant.create!(name: 'Jewelery Rangers')
+
+      expect(Merchant.search_merchant("LiTTle")).to eq(merchant_1)
+      expect(Merchant.search_merchant("rang")).to eq(merchant_3)
+      expect(Merchant.search_merchant("imp")).to eq(merchant_2)
+    end
+  end
 end
